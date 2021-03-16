1bffac928b35cc121f615b96963525fb686d40e5
vineboneto <vineboneto@gmail.com>
2021-03-16 01:20:34 -0300

    test: ensure Login present error and hide spinner on authentication failure

--- tests/presentation/pages/login/login.spec.tsx
+++ tests/presentation/pages/login/login.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import faker from 'faker'
+import { render, RenderResult, cleanup, fireEvent, waitFor } from '@testing-library/react'
 
-import { render, RenderResult, cleanup, fireEvent } from '@testing-library/react'
 import { Login } from '@/presentation/pages'
-
+import { InvalidCredentialsError } from '@/domain/errors'
 import { ValidationStub, AuthenticationSpy } from '@/tests/presentation/mocks'
 
 type SutTypes = {
@@ -100,10 +100,7 @@ describe('Login Component', () => {
 
   test('Should show spinner on submit', () => {
     const { sut } = makeSut()
-    populateEmailField(sut)
-    populatePasswordField(sut)
-    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
-    fireEvent.click(submitButton)
+    simulateValidSubmit(sut)
     const spinner = sut.getByTestId('spinner')
     expect(spinner).toBeTruthy()
   })
@@ -132,5 +129,17 @@ describe('Login Component', () => {
     populateEmailField(sut)
     fireEvent.submit(sut.getByTestId('form'))
     expect(authenticationSpy.callsCount).toBe(0)
+  })
+
+  test('Should present error if Authentication fails', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error))
+    simulateValidSubmit(sut)
+    const errorWrap = sut.getByTestId('error-wrap')
+    await waitFor(() => errorWrap)
+    const mainError = sut.getByTestId('main-error')
+    expect(mainError.textContent).toBe(error.message)
+    expect(errorWrap.childElementCount).toBe(1)
   })
 })
