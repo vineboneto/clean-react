a68f262de36343c2dc3b2cf3c0f8813add175222
vineboneto <vineboneto@gmail.com>
2021-03-16 23:11:50 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ tests/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,28 @@
+import { ValidationComposite } from '@/validation/validators'
+import { FieldValidationSpy } from '@/tests/validation/mocks'
+
+describe('ValidationComposite', () => {
+  test('Should return error if validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('any_error_message')
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('any_error_message')
+  })
+
+  test('Should return error if validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('any_error_message')
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('any_error_message')
+  })
+})
