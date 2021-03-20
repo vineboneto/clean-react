c71b17b261b0e3c00cd5387a23aa6e4a12ccd0f4
vineboneto <vineboneto@gmail.com>
2021-03-19 22:26:17 -0300

    test: ensure CompareFieldValidation returns error if validation fails

---
+++ tests/validation/validators/compare-fields/compare-field.spec.ts
@@ -0,0 +1,16 @@
+import { CompareFieldsValidation } from '@/validation/validators'
+import { InvalidFieldError } from '@/validation/errors'
+
+import faker from 'faker'
+
+const makeSut = (valueToCompare: string): CompareFieldsValidation => (
+  new CompareFieldsValidation(faker.database.column(), valueToCompare)
+)
+
+describe('CompareFieldsValidation', () => {
+  test('Should return error if compare is invalid', () => {
+    const sut = makeSut(faker.random.word())
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
