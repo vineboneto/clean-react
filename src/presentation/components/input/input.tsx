5801b117801065f95573414bc8ee1bffb92b7b25
vineboneto <vineboneto@gmail.com>
2021-03-23 07:49:41 -0300

    refactor: improve layout

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -13,26 +13,29 @@ const Input: React.FC<Props> = (props: Props) => {
     setState({ ...state, [e.target.name]: e.target.value })
 
   return (
-    <div className={Styles.inputWrap}>
+    <div
+      className={Styles.inputWrap}
+      data-status={error ? 'invalid' : 'valid'}
+      data-testid={`${props.name}-wrap`}
+    >
       <input
         { ...props }
         ref={inputRef}
+        title={error}
         placeholder=" "
         data-testid={props.name}
         readOnly
         onFocus={e => { e.target.readOnly = false }}
         onChange={handleChange}
+        autoComplete="off"
       />
-      <label onClick={() => { inputRef.current.focus() }}>
+      <label
+        data-testid={`${props.name}-label`}
+        onClick={() => { inputRef.current.focus() }}
+        title={error}
+      >
         {props.placeholder}
       </label>
-      <span
-        data-testid={`${props.name}-status`}
-        title={error || 'Tudo Certo!'}
-        className={Styles.status}
-      >
-        {error ? '🔴' : '🟢'}
-      </span>
     </div>
   )
 }
