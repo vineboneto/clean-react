7279117a3a73df96afa0b962b0c7b7beb20c02aa
vineboneto <vineboneto@gmail.com>
2021-03-23 07:19:29 -0300

    test: ensure Login will not submit form  if its invalid

--- tests/main/cypress/integration/login.spec.ts
+++ tests/main/cypress/integration/login.spec.ts
@@ -120,4 +120,15 @@ describe('Login', () => {
     cy.getByTestId('submit').dblclick()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not submit if form is invalid', () => {
+    cy.intercept('POST', localServerUrl, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.random.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
