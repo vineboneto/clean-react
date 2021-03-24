5cb029181c77c48926d2e0e37ac49ea605b5c6d5
vineboneto <vineboneto@gmail.com>
2021-03-24 12:15:50 -0300

    feat: ensure RemoteLoadSurveyList returns a list on 200

--- src/domain/usecases/survey-list.ts
+++ src/domain/usecases/survey-list.ts
@@ -1,9 +1,5 @@
 import { SurveyModel } from '@/domain/models'
 
 export interface LoadSurveyList {
-  loadAll (): Promise<LoadSurveyList.Result>
-}
-
-export namespace LoadSurveyList {
-  export type Result = SurveyModel[]
+  loadAll (): Promise<SurveyModel[]>
 }
