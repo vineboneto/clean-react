1a594f60977836f0bc892da42f0b88bb2fb89353
vineboneto <vineboneto@gmail.com>
2021-03-25 20:31:07 -0300

    feat: add survey icon to show status

---
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Styles from './survey-list-styles.scss'
+
+import { Footer, Logo } from '@/presentation/components'
+
+const SurveyList: React.FC = () => {
+  return (
+    <div className={Styles.surveyListWrap}>
+      <header className={Styles.headerWrap}>
+        <div className={Styles.headerContent}>
+          <Logo />
+          <div className={Styles.logoutWrap}>
+            <span>Rodrigo</span>
+            <a href="#">sair</a>
+          </div>
+        </div>
+      </header>
+
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul>
+          <li>
+            <div className={Styles.surveyContent}>
+              <div className={[Styles.iconWrap, Styles.green].join(' ')}>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII=" alt="Like"/>
+              </div>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é o seu framework web favorito ?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+        </ul>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyList
