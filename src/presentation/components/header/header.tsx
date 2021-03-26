10cf434dd5b818690659518064748e4233961a67
vineboneto <vineboneto@gmail.com>
2021-03-25 21:13:37 -0300

    refactor: move header its own file

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,20 @@
+import React, { memo } from 'react'
+import Styles from './header-styles.scss'
+
+import Logo from '@/presentation/components/logo/logo'
+
+const Header: React.FC = () => {
+  return (
+    <header className={Styles.headerWrap}>
+        <div className={Styles.headerContent}>
+          <Logo />
+          <div className={Styles.logoutWrap}>
+            <span>Rodrigo</span>
+            <a href="#">sair</a>
+          </div>
+        </div>
+    </header>
+  )
+}
+
+export default memo(Header)
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import Styles from './survey-list-styles.scss'
 
-import { Footer, Logo } from '@/presentation/components'
+import { Footer, Header } from '@/presentation/components'
 
 const SurveyList: React.FC = () => {
   return (
     <div className={Styles.surveyListWrap}>
-      <header className={Styles.headerWrap}>
-        <div className={Styles.headerContent}>
-          <Logo />
-          <div className={Styles.logoutWrap}>
-            <span>Rodrigo</span>
-            <a href="#">sair</a>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
