ead250aedac64b6c9f9b7ca16f6bed86365acf38
vineboneto <vineboneto@gmail.com>
2021-04-02 17:42:00 -0300

    refactor: change SurveyList to use custom hook

---
+++ src/presentation/hooks/use-error-handler.ts
@@ -0,0 +1,21 @@
+import { AccessDeniedError } from '@/domain/errors'
+import { ApiContext } from '@/presentation/contexts'
+
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+
+type CallBackType = (error: Error) => void
+type ResultType = CallBackType
+
+export const useErrorHandler = (callback: CallBackType): ResultType => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
+  return (error: Error): void => {
+    if (error instanceof AccessDeniedError) {
+      setCurrentAccount(undefined)
+      history.replace('/login')
+    } else {
+      callback(error)
+    }
+  }
+}
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,21 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './survey-list-styles.scss'
 
-import { ApiContext } from '@/presentation/contexts'
 import { Footer, Header } from '@/presentation/components'
 import { SurveyListItem, SurveyError } from '@/presentation/pages/survey-list/components'
 import { SurveyContext } from '@/presentation/pages/survey-list/context'
 import { LoadSurveyList } from '@/domain/usecases'
-import { AccessDeniedError } from '@/domain/errors'
-import { useHistory } from 'react-router-dom'
+import { useErrorHandler } from '@/presentation/hooks'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
-  const history = useHistory()
-  const { setCurrentAccount } = useContext(ApiContext)
+  const handleError = useErrorHandler((error: Error) => {
+    setState({ ...state, error: error.message })
+  })
   const [state, setState] = useState({
     surveys: [] as LoadSurveyList.Model[],
     error: '',
@@ -24,14 +23,7 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   useEffect(() => {
     loadSurveyList.loadAll()
       .then(surveys => setState({ ...state, surveys }))
-      .catch(error => {
-        if (error instanceof AccessDeniedError) {
-          setCurrentAccount(undefined)
-          history.replace('/login')
-        } else {
-          setState({ ...state, error: error.message })
-        }
-      })
+      .catch(error => handleError(error))
   }, [state.reload])
 
   return (
