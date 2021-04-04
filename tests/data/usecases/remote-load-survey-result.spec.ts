9b43d67248e502a76c31ceb994c59f91c60bf3fb
vineboneto <vineboneto@gmail.com>
2021-04-04 17:52:41 -0300

    test: ensure RemoteLoadSurveyResult throw AccessDeniedError if HttpGetClient returns 403

--- tests/data/usecases/remote-load-survey-result.spec.ts
+++ tests/data/usecases/remote-load-survey-result.spec.ts
@@ -1,4 +1,5 @@
 import { RemoteLoadSurveyResult } from '@/data/usecases'
+import { HttpStatusCode } from '@/data/protocols'
 import { HttpGetClientSpy } from '@/tests/data/mocks'
 import faker from 'faker'
 
@@ -22,5 +23,14 @@ describe('RemoteLoadSurveyResult', () => {
     const { sut, httpGetClientSpy } = makeSut(url)
     await sut.load()
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
   })
 })
