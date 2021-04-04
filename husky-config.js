54aa695893b9cba05252d1e925773fac0913f893
vineboneto <vineboneto@gmail.com>
2021-04-04 13:09:49 -0300

    chore: config webpack prod and dev

---
+++ husky-config.js
@@ -0,0 +1,20 @@
+const { execSync } = require('child_process')
+
+console.log('Setting Husky...')
+const scripts = [
+  'npx rimraf .config',
+  'npx husky install .config/husky',
+  'npx husky add .config/husky/pre-push "yarn test:ci"',
+  'npx husky add .config/husky/pre-commit "yarn lint-staged"',
+  'npx husky add .config/husky/commit-msg ".git/hooks/commit-msg $1"'
+]
+
+for (script of scripts) {
+  try {
+    console.log(script + '👀')
+    execSync(script)
+    console.log('✔')
+  } catch (error) {
+    console.error
+  }
+}
