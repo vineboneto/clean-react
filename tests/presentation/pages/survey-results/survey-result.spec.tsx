ff1214fd89e83de6e7815005dd9eab45d057082d
vineboneto <vineboneto@gmail.com>
2021-04-05 11:05:27 -0300

    test: ensure SurveyResult goto SurveyList on back button click

--- tests/presentation/pages/survey-results/survey-result.spec.tsx
+++ tests/presentation/pages/survey-results/survey-result.spec.tsx
@@ -16,7 +16,7 @@ type SutTypes = {
 }
 
 const makeSut = (loadSurveyResultsSpy = new LoadSurveyResultSpy()): SutTypes => {
-  const history = createMemoryHistory()
+  const history = createMemoryHistory({ initialEntries: ['/', '/surveys/any_id'], initialIndex: 1 })
   const setCurrentAccountMock = jest.fn()
   render(
     <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
@@ -109,5 +109,12 @@ describe('SurveyResult Component', () => {
     fireEvent.click(screen.getByTestId('reload'))
     expect(loadSurveyResultSpy.callsCount).toBe(1)
     await waitFor(() => screen.getByTestId('survey-result'))
+  })
+
+  test('Should goto SurveyList on back button click', async () => {
+    const { history } = makeSut()
+    await waitFor(() => screen.getByTestId('survey-result'))
+    fireEvent.click(screen.getByTestId('back-button'))
+    expect(history.location.pathname).toBe('/')
   })
 })
