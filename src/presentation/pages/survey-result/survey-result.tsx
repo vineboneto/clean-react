96df7db84ef9f5e2518568c509f266b39c92ed96
vineboneto <vineboneto@gmail.com>
2021-04-06 11:29:38 -0300

    feat: ensure SurveyResult prevent multiple answer click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -24,6 +24,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult, saveSurveyResult }: P
   })
 
   const onAnswer = (answer: string): void => {
+    if (state.isLoading) return
     setState(old => ({ ...old, isLoading: true }))
     saveSurveyResult.save({ answer })
       .then(surveyResult => setState(old => ({ ...old, surveyResult, isLoading: false })))
