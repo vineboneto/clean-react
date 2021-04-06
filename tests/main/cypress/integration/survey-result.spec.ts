44df1e19d969d3a4d734bf2104bf80e02eb0ac92
vineboneto <vineboneto@gmail.com>
2021-04-06 11:40:09 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- tests/main/cypress/integration/survey-result.spec.ts
+++ tests/main/cypress/integration/survey-result.spec.ts
@@ -70,6 +70,7 @@ describe('SurveyResult', () => {
 
   describe.only('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+    const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT')
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -83,6 +84,12 @@ describe('SurveyResult', () => {
       mockUnexpectedError()
       cy.get('li:nth-child(2)').click()
       cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde')
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      Helper.testUrl('/login')
     })
   })
 })
