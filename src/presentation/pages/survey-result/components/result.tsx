b7d7e6d1f9313692d94dcffd73bc44f109fadde2
vineboneto <vineboneto@gmail.com>
2021-04-06 11:54:24 -0300

    refactor: remove flip move

--- src/presentation/pages/survey-result/components/result.tsx
+++ src/presentation/pages/survey-result/components/result.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import FlipMove from 'react-flip-move'
 import { useHistory } from 'react-router-dom'
 import Styles from './result-styles.scss'
 import { LoadSurveyResult } from '@/domain/usecases'
@@ -18,11 +17,9 @@ const Result: React.FC<Props> = ({ surveyResult }: Props) => {
         <Calendar date={surveyResult.date} className={Styles.calendarWrap} />
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
-      <FlipMove data-testid="answers" className={Styles.answersList}>
-        <>
-          {surveyResult.answers.map(answer => <Answer key={answer.answer} answer={answer} />)}
-        </>
-      </FlipMove>
+      <ul data-testid="answers" className={Styles.answersList}>
+        {surveyResult.answers.map(answer => <Answer key={answer.answer} answer={answer} />)}
+      </ul>
       <button data-testid="back-button" onClick={goBack} className={Styles.button}>
         Voltar
       </button>
